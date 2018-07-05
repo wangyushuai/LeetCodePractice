e5d9d2398f432cf53e9ccc6d8480a7855b9d9766
wangyushuai <wangyushuai@fang.com>
2018-07-05 13:53:20 +0800

    LeetCode8 字符串转整型数字

--- src/main/java/LeetCode8.java
+++ src/main/java/LeetCode8.java
@@ -27,4 +27,46 @@ public class LeetCode8 {
         return result;
     }
 
+    /**
+     * 字符串转数字
+     * @param str
+     * @return
+     */
+    public int myAtoi(String str) {
+        char[] chars = str.toCharArray();
+        boolean isStart = false;
+        boolean isHaveSign = false;
+        int sign = 1;
+        long result = 0;
+
+        for (char c : chars) {
+            if (c == ' ' && !isStart) {
+                continue;
+            } else if ( (c == '-'|| c== '+') && !isHaveSign && !isStart) {
+                isHaveSign = true;
+                isStart = true;
+                sign = c== '-'? -1 : 1;
+                continue;
+            } else if ( (c >= '0' && c <= '9') || (c == '0' && isStart)) {
+                long temp =  result * 10 + c -'0';
+                if (sign * temp > Integer.MAX_VALUE) {
+                    return Integer.MAX_VALUE;
+                } else if(sign * temp < Integer.MIN_VALUE) {
+                    return  Integer.MIN_VALUE;
+                } else {
+                    result = result * 10 + c -'0';
+                }
+                isStart = true;
+
+            }else if(c != ' ' && (c < '0' || c > '9')  && !isStart ) {
+                return 0;
+            }else if (!isStart) {
+                continue;
+            } else {
+                break;
+            }
+        }
+        return (int)(result*sign);
+    }
+
 }
--- src/test/java/LeetCode8Test.java
+++ src/test/java/LeetCode8Test.java
@@ -20,4 +20,23 @@ public class LeetCode8Test {
         String testStr = "42";
         Assert.assertTrue(4193 == leetCode8.stringToInteger(testStr));
     }
+
+    @Test
+    public void myAtoiTest() throws Exception {
+         int except = 0;
+         int value = leetCode8.myAtoi("words and 987");
+         Assert.assertTrue(except == value );
+         int value1 = leetCode8.myAtoi("-91283472332");
+         Assert.assertTrue("整数最大最小值测试失败 expect:-2147483648, actrullay:" + value1,-2147483648 == value1);
+         //Assert.assertTrue("010TestError",0 == leetCode8.myAtoi("010"));
+         Assert.assertTrue("符号位测试", 1 == leetCode8.myAtoi("+1"));
+         Assert.assertTrue("0-1测试失败", 0 == leetCode8.myAtoi("0-1"));
+    }
+
+
+
+
+
+
+
 }
