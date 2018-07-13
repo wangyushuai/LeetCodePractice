090d350f70ad42078f8f5de3c244a4a00ae97ce0
wangyushuai <wangyushuai@fang.com>
2018-07-13 11:03:12 +0800

    LeetCode10 正则匹配

---
+++ src/main/java/LeetCode10.java
@@ -0,0 +1,27 @@
+/**
+ * Created by wangyushuai@fang.com on 2018/7/12.
+ */
+public class LeetCode10 {
+    /*
+    * 正则匹配
+    * */
+    public boolean isMatch(String s,String p) {
+        if (p.length() == 0) {
+            return s.length() == 0;
+        }
+        if (p.length() > 1 && p.charAt(1) == '*') {  // second char is '*'
+            if (isMatch(s, p.substring(2))) {
+                return true;
+            }
+            if(s.length() > 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))) {
+                return isMatch(s.substring(1), p);
+            }
+            return false;
+        } else {                                     // second char is not '*'
+            if(s.length() > 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))) {
+                return isMatch(s.substring(1), p.substring(1));
+            }
+            return false;
+        }
+    }
+}
---
+++ src/test/java/LeetCode10Test.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wangyushuai@fang.com on 2018/7/13.
+ */
+public class LeetCode10Test {
+    private LeetCode10 leetCode10;
+    @Before
+    public void setUp() throws Exception {
+        leetCode10 = new LeetCode10();
+    }
+
+    @Test
+    public void isMatch() throws Exception {
+        String str1 = "aab";
+        String p1 = "c*a*b";
+        boolean isTrue1 = leetCode10.isMatch(str1,p1);
+        Assert.assertTrue(isTrue1);
+    }
+}
