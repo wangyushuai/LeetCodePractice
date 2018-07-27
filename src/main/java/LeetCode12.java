a5b54e0e7ea2444a131890a7fe1fd5b4a9a5b734
wangyushuai <wangyushuai@fang.com>
2018-07-27 16:55:51 +0800

    LeetCode12 整形 转罗马

---
+++ src/main/java/LeetCode12.java
@@ -0,0 +1,16 @@
+/**
+ * Created by wangyushuai@fang.com on 2018/7/27.
+ * Integer to Roman
+ */
+public class LeetCode12 {
+
+    private static String[] M = {"","M","MM","MMM"};
+    private static String[] C = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    private static String[] X = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    private static String[] I = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+
+    public  String intToRoman(int num) {
+        if (num <0 || num > 3999) return "";
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[(num%10)];
+    }
+}
---
+++ src/test/java/LeetCode12Test.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wangyushuai@fang.com on 2018/7/27.
+ */
+public class LeetCode12Test {
+    private LeetCode12 leetCode12;
+
+    @Before
+    public void setUp() throws Exception {
+        leetCode12 = new LeetCode12();
+    }
+
+    @Test
+    public void intToRoman() throws Exception {
+        Assert.assertTrue("change test","III" == leetCode12.intToRoman(3));
+        Assert.assertTrue("change test","IV" ==leetCode12.intToRoman(4));
+        Assert.assertTrue("change test","LVIII" == leetCode12.intToRoman(58));
+        Assert.assertTrue("change test","MCMXCIV" == leetCode12.intToRoman(1994));
+    }
+
+    @Test
+    public void whenInputOutRangeThenRetrun0() {
+        String result = leetCode12.intToRoman(-1);
+        Assert.assertSame("input exception test","",result);
+    }
+
+
+
+}
