843719d0069005d7057b5dc2421e9b81ee05a59d
wangyushuai <wangyushuai2@jd.com>
2020-10-22 20:43:13 +0800

    LeetCode242：有效字符异位词

---
+++ src/main/java/LeetCode242.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/**
+ * 有效字符异位词
+ * @author wangyushuai2@jd.com
+ * @date 2020/10/22
+ */
+public class LeetCode242 {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return  Arrays.equals(sChars,tChars);
+    }
+}
