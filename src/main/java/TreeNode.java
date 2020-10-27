88d440f4e2747248ecc5f444e2c0baefa8fc5658
wangyushuai <wangyushuai2@jd.com>
2020-10-27 14:53:18 +0800

    添加树基本结构

---
+++ src/main/java/TreeNode.java
@@ -0,0 +1,16 @@
+/**
+ * 树
+ * @author wangyushuai2@jd.com
+ * @date 2020/10/27
+ */
+public class TreeNode {
+    public int val;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
