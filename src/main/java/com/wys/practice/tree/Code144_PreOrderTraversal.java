465f87af526a3397809d1b1c0d2a152cbb1166ee
wangyushuai <wangyushuai2@jd.com>
2020-10-27 16:04:29 +0800

    LeetCode94: 前序遍历

---
+++ src/main/java/com/wys/practice/tree/Code144_PreOrderTraversal.java
@@ -0,0 +1,39 @@
+package com.wys.practice.tree;
+
+import com.wys.practice.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author wangyushuai2@jd.com
+ * @date 2020/10/27
+ */
+public class Code144_PreOrderTraversal {
+    /**
+     * 前序遍历
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (! stack.isEmpty() || node != null) {
+            while (node != null) {
+                result.add(node.val);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        return result;
+    }
+
+}
