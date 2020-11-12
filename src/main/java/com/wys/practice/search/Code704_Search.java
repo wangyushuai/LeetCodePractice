60f7f6ab51c97f8ad7c98db5837ec2cb5725404a
wangyushuai <wangyushuai2@jd.com>
2020-11-12 18:52:18 +0800

    二分查找添加测试用例

--- src/main/java/com/wys/practice/search/Code704_Search.java
+++ src/main/java/com/wys/practice/search/Code704_Search.java
@@ -1,5 +1,8 @@
 package com.wys.practice.search;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * 二分查找
  * @author wangyushuai2@jd.com
@@ -23,8 +26,13 @@ public class Code704_Search {
         return -1;
     }
 
+    @Test
     public void test() {
-
+        int[] arr = {-1,0,3,5,9,12};
+        int i = search(arr,3);
+        int j = search(arr,9);
+        Assert.assertEquals("二分查找-1",2,i);
+        Assert.assertEquals("二分查找-2",4,j);
     }
 
 
