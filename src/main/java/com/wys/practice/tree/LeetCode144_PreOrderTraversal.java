2a1c4f0e1ac6783e5807b3270c3acdd2b39a21af
wangyushuai <wangyushuai2@jd.com>
2020-12-07 14:53:29 +0800

    LeetCode144: 重写二叉树中序遍历

--- src/main/java/com/wys/practice/tree/LeetCode144_PreOrderTraversal.java
+++ src/main/java/com/wys/practice/tree/LeetCode144_PreOrderTraversal.java
@@ -1,14 +1,15 @@
 package com.wys.practice.tree;
 
 import com.wys.practice.TreeNode;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
- * 二叉树中序遍历
+ * 二叉树前序遍历
+ * 递归思路
  * @author wangyushuai2@jd.com
  * @date 2020/10/27
  */
@@ -18,23 +19,35 @@ public class LeetCode144_PreOrderTraversal {
      * @param root
      * @return
      */
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
+    public List<Integer> preOrderTraversal(TreeNode root) {
         if (root == null) {
-            return result;
+            return new ArrayList<>();
         }
-        Deque<TreeNode> stack = new LinkedList<>();
-        TreeNode node = root;
-        while (! stack.isEmpty() || node != null) {
-            while (node != null) {
-                result.add(node.val);
-                stack.push(node);
-                node = node.left;
-            }
-            node = stack.pop();
-            node = node.right;
+        List<Integer> result = new ArrayList<>();
+        result.add(root.val);
+        if (root.left != null) {
+            result.addAll(preOrderTraversal(root.left));
+        }
+        if (root.right != null) {
+            result.addAll(preOrderTraversal(root.right));
         }
         return result;
+    }
+
+    @Test
+    public void test() {
+        TreeNode treeNode = new TreeNode(1, null,
+                new TreeNode(2,
+                        new TreeNode(3,null,null),
+                        null));
+        List<Integer> result = preOrderTraversal(treeNode);
+        String excepted = StringUtils.join(Arrays.asList(1,2,3),",");
+        Assert.assertEquals("二叉树先序遍历",excepted,StringUtils.join(result,","));
+
+        TreeNode treeNode5 = new TreeNode(1,
+                new TreeNode(2, null, null),null);
+        List<Integer> result5 = preOrderTraversal(treeNode5);
+        Assert.assertEquals("二叉树先序遍历","1,2",StringUtils.join(result5,","));
     }
 
 }
